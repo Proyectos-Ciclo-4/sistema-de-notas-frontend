ea16156770f42a381be7e19409c6c4382187f9c2
camora1990 <moorales.camilo@gmail.com>
2022-09-20 15:38:53 -0500

    [refactoring] module prime-ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PrimeNgModule } from './prime-ng/prime-ng/prime-ng.module';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
