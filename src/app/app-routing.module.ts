c6a8efff1615e23db62eab3cc41ce120ee5d8822
camora1990 <moorales.camilo@gmail.com>
2022-09-20 23:57:45 -0500

    [add] route protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    ...canActivate(() => redirectLoggedInTo(['/sofkau-note'])),
   },
   {
-    path:'sofkau-note',
-    loadChildren: ()=> import("./sofka-note/sofka-note.module").then(m=>m.SofkaNoteModule)
-  }
+    path: 'sofkau-note',
+    loadChildren: () =>
+      import('./sofka-note/sofka-note.module').then((m) => m.SofkaNoteModule),
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
+  },
 ];
 
 @NgModule({
