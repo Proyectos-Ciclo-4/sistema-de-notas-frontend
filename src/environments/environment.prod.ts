7d88c0a16d614287cf86f797e7268d5e9491d035
camora1990 <moorales.camilo@gmail.com>
2022-09-20 22:16:01 -0500

    [add] firebase configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'sofkau-note',
+    appId: '1:471437960998:web:5a91853244da843f2c03e7',
+    storageBucket: 'sofkau-note.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB8cRLwjer9ijbw0n_U3P-iGzx-WSUQk6U',
+    authDomain: 'sofkau-note.firebaseapp.com',
+    messagingSenderId: '471437960998',
+  },
   production: true
 };
