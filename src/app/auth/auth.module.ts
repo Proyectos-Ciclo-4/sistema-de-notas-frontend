9370f1e26578ac614bd6fb6f52fd84bbff04ce5f
camora1990 <moorales.camilo@gmail.com>
2022-09-20 23:58:29 -0500

    [add] modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule, PrimeNgModule, SharedModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    PrimeNgModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class AuthModule {}
