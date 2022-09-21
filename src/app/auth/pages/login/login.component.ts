46d5c4765239d0005dffecdb243229f0f93ecac5
camora1990 <moorales.camilo@gmail.com>
2022-09-21 00:01:36 -0500

    [add] login and reset user

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { SweetalertService } from '../../../shared/service/sweetalert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  formLogin: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private swal$: SweetalertService,
+    private router: Router
+  ) {
+    this.formLogin = this.createFormLogin();
   }
 
+  createFormLogin(): FormGroup<any> {
+    return new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  login() {
+    this.authService
+      .login(this.formLogin.value)
+      .then((res) =>
+        this.swal$
+          .succesMessage(`Welcome ${res.user.email!}`)
+          .then(() => this.router.navigate(['/sofkau-note']))
+      )
+      .catch((err) => {
+        this.swal$.errorMessage(err.code);
+        console.log(err);
+      });
+  }
+
+  ngOnInit(): void {}
+
+  resetPassword() {
+    this.swal$.resetPassword();
+  }
 }
