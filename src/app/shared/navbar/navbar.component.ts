0e68b9d41589330874ffefd428f788af6b0a05c1
Nestor Quiroga <nqs48@icloud.com>
2022-09-23 08:50:08 -0500

    Adding function logout with message

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,24 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '@angular/fire/auth';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 import { UserModel } from 'src/app/auth/interface/user.model';
 import { AuthService } from 'src/app/auth/services/auth.service';
+
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
+import { SweetalertService } from '../service/sweetalert.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  styles: [
+    `
+      :host ::ng-deep .ui-slidemenu {
+        width: 13.5em;
+      }
+    `,
+  ],
 })
+
 export class NavbarComponent implements OnInit {
-  faEnvelope = faEnvelope;
-
   currentLogin!: UserModel;
+  items!: MenuItem[];
 
-  constructor(private authservice: AuthService) {}
+  constructor(
+    private authservice: AuthService,
+    private router: Router,
+    private swal$: SweetalertService
+  ) {}
 
   ngOnInit(): void {
     this.authservice.currentUser().subscribe((user) => {
       this.currentLogin = user[0];
+    });
+
+    this.items = [
+      { label: 'Perfil', icon: 'pi pi-user-edit' },
+      { label: 'Configuración', icon: 'pi pi-pencil' },
+      { separator: true },
+      {
+        label: 'Logout',
+        icon: 'pi pi-power-off',
+        command: () => {
+          this.logout();
+        },
+      },
+    ];
+  }
+
+
+  logout() {
+    this.authservice.logout().then(() => {
+      this.swal$.succesMessage('Sesión cerrada correctamente').then(() => {
+        this.router.navigate(['/auth/login']);
+      });
     });
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,23 @@ import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
+//PrimeNG
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToastModule } from 'primeng/toast';
+
 @NgModule({
   declarations: [HeaderComponent, NavbarComponent],
-  imports: [CommonModule, FontAwesomeModule, PrimeNgModule],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    PrimeNgModule,
+    SlideMenuModule,
+    ButtonModule,
+    SplitButtonModule,
+    ToastModule,
+  ],
   exports: [HeaderComponent, NavbarComponent],
 })
 export class SharedModule {}
