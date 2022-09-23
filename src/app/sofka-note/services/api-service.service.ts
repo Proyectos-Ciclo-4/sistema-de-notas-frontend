1062da2fe1cf26a66573e53528b8116ee931c761
camora1990 <moorales.camilo@gmail.com>
2022-09-23 10:09:21 -0500

    [add] topic service

--- src/app/sofka-note/services/api-service.service.ts
+++ src/app/sofka-note/services/api-service.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CourseModel } from '../interfaces/course.model';
+import { TopicModel } from '../interfaces/topic.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiServiceService {
   private courses: CourseModel[];
+  private topics: TopicModel[];
 
   constructor() {
     this.courses = [
@@ -35,9 +37,30 @@ export class ApiServiceService {
         titulo: 'abcd',
       },
     ];
+    this.topics = [
+      {
+        temaID: '1',
+        orden: 2,
+        titulo: 'titulo # 1',
+      },
+      {
+        temaID: '2',
+        orden: 2,
+        titulo: 'titulo # 2',
+      },
+      {
+        temaID: '1',
+        orden: 3,
+        titulo: 'titulo # 3',
+      },
+    ];
   }
 
   searchCourse(term: string) {
     return this.courses.filter((e) => e.titulo.includes(term));
+  }
+
+  getTopic(courseId:string) {
+    return this.topics
   }
 }
