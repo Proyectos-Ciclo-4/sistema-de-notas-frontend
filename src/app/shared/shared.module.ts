c1a37bc189061ff68bbee4c900256a1a401399a0
Nestor Quiroga <nqs48@icloud.com>
2022-09-22 19:40:30 -0500

    Updating styles to navbar & header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FontAwesomeModule
-} from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 // components
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [HeaderComponent, NavbarComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, PrimeNgModule],
   exports: [HeaderComponent, NavbarComponent],
 })
 export class SharedModule {}
