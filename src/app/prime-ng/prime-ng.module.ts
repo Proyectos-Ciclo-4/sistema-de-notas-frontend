fc6eb18ca6af5fbd7485057df36db618b6357ce6
camora1990 <moorales.camilo@gmail.com>
2022-09-22 22:30:31 -0500

    [add] tabviewmodel primeNg

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,10 +6,10 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import {DropdownModule} from 'primeng/dropdown';
 import {TabViewModule} from 'primeng/tabview';
-
+import {DividerModule} from 'primeng/divider';
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, InputTextModule, DropdownModule,TabViewModule],
+  exports: [ButtonModule, InputTextModule, DropdownModule,TabViewModule,DividerModule],
 })
 export class PrimeNgModule {}
