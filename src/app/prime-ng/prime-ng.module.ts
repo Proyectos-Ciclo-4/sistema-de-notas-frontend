175138157b3b309d414976fe5627af5bf5f2586d
camora1990 <moorales.camilo@gmail.com>
2022-09-23 01:10:37 -0500

    [add] dialogModule primeNg

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 
-
 // components primeNG
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import {DropdownModule} from 'primeng/dropdown';
-import {TabViewModule} from 'primeng/tabview';
-import {DividerModule} from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { TabViewModule } from 'primeng/tabview';
+import { DividerModule } from 'primeng/divider';
+import { DialogModule } from 'primeng/dialog';
+
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, InputTextModule, DropdownModule,TabViewModule,DividerModule],
+  exports: [
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    TabViewModule,
+    DividerModule,
+    DialogModule
+  ],
 })
 export class PrimeNgModule {}
