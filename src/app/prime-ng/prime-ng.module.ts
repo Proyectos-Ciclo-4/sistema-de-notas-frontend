33776f76de72510a8d4e8d22ccbc74e1750ae5ac
Nestor Quiroga <nqs48@icloud.com>
2022-09-23 10:20:03 -0500

    Refactoring imports NgModule

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 
-
 // components primeNG
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import {DropdownModule} from 'primeng/dropdown';
+import { DropdownModule } from 'primeng/dropdown';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, InputTextModule, DropdownModule],
+  exports: [
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    SlideMenuModule,
+    SplitButtonModule,
+    ToastModule,
+  ],
 })
 export class PrimeNgModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,29 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 // components
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
-//PrimeNG
-import { SlideMenuModule } from 'primeng/slidemenu';
-import { ButtonModule } from 'primeng/button';
-import { SplitButtonModule } from 'primeng/splitbutton';
-import { ToastModule } from 'primeng/toast';
-
 @NgModule({
   declarations: [HeaderComponent, NavbarComponent],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    PrimeNgModule,
-    SlideMenuModule,
-    ButtonModule,
-    SplitButtonModule,
-    ToastModule,
-  ],
+  imports: [CommonModule, PrimeNgModule],
   exports: [HeaderComponent, NavbarComponent],
 })
 export class SharedModule {}
