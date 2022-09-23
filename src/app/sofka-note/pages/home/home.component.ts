c12ec7bd846cddb7528e918235d6c1d41a8370ff
camora1990 <moorales.camilo@gmail.com>
2022-09-23 10:12:05 -0500

    [add] create course section

--- src/app/sofka-note/pages/home/home.component.ts
+++ src/app/sofka-note/pages/home/home.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../../services/api-service.service';
+import { CourseModel } from '../../interfaces/course.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  headerStyle: any;
+  course?: CourseModel | null;
+  termSearch: string = '';
+  courses: CourseModel[] = [];
+  showSuggestion: boolean = false;
+  showDialog: boolean = false;
+  showTask: boolean = false;
+  showCreateCourse: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private api$: ApiServiceService) {
+    this.headerStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      width: '33.33%',
+      fontWeight: 'bold',
+    };
   }
 
+  ngOnInit(): void {}
+
+  searchCourse(termSearch: string) {
+    this.courses = this.api$.searchCourse(termSearch);
+  }
+
+  courseSuggestions(termSearch: string) {
+    this.course = null;
+    this.termSearch = termSearch;
+    this.showSuggestion = true;
+    if (termSearch != '') {
+      this.courses = this.api$.searchCourse(termSearch);
+    } else {
+      this.courses = [];
+    }
+  }
+
+  selectCourse(course: CourseModel) {
+    this.termSearch = course.titulo;
+    this.course = course;
+    this.courses = [];
+    this.showSuggestion = false;
+  }
+
+  createCourse() {
+    this.showDialog = true;
+  }
+
+  closeModal(event: boolean) {
+    this.showDialog = event;
+  }
+
+  showAccordion(show: boolean) {
+    this.showCreateCourse = !this.showCreateCourse;
+    this.showTask = !this.showTask;
+  }
 }
