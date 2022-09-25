6b9d6b11df52363d324a4dca851665c81460d87d
camora1990 <moorales.camilo@gmail.com>
2022-09-24 20:02:26 -0500

    [add] courses mocks

---
+++ src/assets/db/courses.ts
@@ -0,0 +1,326 @@
+export const data = [
+  {
+    _id: '632e19dd6360822de7a9bbce',
+    titulo:
+      'Nostrud et occaecat aute cupidatat sit laborum id eiusmod proident ex ipsum',
+    profesorID: '632e19ddd841492b68f0a5a2',
+    temas: [
+      {
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 8 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo: '1',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },
+      {
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },
+      {
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 10 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },
+      {
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },{
+        temaID: '632e19dd08bee068ab4aa81c',
+        cursoID: '632e19dd6b164ab31cbb933e',
+        titulo: 'Hello, undefined! You have 9 unread messages.',
+        orden: 1,
+        tareas: [
+          {
+            _id: '632e19ddf14396af03e192a9',
+            cursoID: '632e19dd618d7b28b07317f6',
+            temaID: '632e19ddabcde35e1389a73c',
+            titulo:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corporis quam, officiis, voluptatem voluptatum ipsum temporibus voluptatibus commodi provident nisi, voluptas aut ad! Aperiam vel recusandae minus voluptas sapiente modi?',
+            fechaLimite: '2022-09-04T04:20:16 +05:00',
+            porcentaje: 9.214,
+          },
+          {
+            _id: '632e19ddbfdba0d3540e51f8',
+            cursoID: '632e19dd97872688ebe25d6c',
+            temaID: '632e19dd61ed6dd11a5e7970',
+            titulo: '4',
+            fechaLimite: '2021-01-15T11:44:26 +05:00',
+            porcentaje: 56.2992,
+          },
+        ],
+      },
+    ],
+  },
+];
