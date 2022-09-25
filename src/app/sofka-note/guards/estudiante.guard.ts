5bff61dfb6359f881a1e40f72df0d7b3138ea2cb
Nestor Quiroga <nqs48@icloud.com>
2022-09-24 19:06:53 -0500

    Adding verification method to guard by Role <Estudiante>

--- src/app/sofka-note/guards/estudiante.guard.ts
+++ src/app/sofka-note/guards/estudiante.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { Role, UserModel } from 'src/app/auth/interface/user.model';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EstudianteGuard implements CanActivate {
+  currentLogin!: UserModel;
+  estudiante: Role = Role.Estudiante;
+
+  constructor(private authService: AuthService) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    // Código de verificación de rol
+    this.authService.currentUser().subscribe((user) => {
+      this.currentLogin = user[0];
+    });
+
+    return this.currentLogin.rol == this.estudiante ? true : false;
   }
-  
 }
