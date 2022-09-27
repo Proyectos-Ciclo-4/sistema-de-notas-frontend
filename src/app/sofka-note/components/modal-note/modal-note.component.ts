d73ea86f2c82c0b6fba9a2d0c840c000911a11a0
Nestor Quiroga <nqs48@icloud.com>
2022-09-27 10:49:24 -0500

    Adding notification to confirm action

--- src/app/sofka-note/components/modal-note/modal-note.component.ts
+++ src/app/sofka-note/components/modal-note/modal-note.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { SweetalertService } from 'src/app/shared/service/sweetalert.service';
 
 @Component({
   selector: 'app-modal-note',
@@ -13,12 +14,25 @@ export class ModalNoteComponent implements OnInit {
 
   formCreateCourse!: FormGroup;
 
-  constructor() {}
+  constructor(private swal$: SweetalertService) {}
 
   ngOnInit(): void {}
 
   closeModalEmmiter() {
     this.closeModal.emit(false);
     this.formCreateCourse.reset();
+  }
+
+  submitForm() {
+    const title = '¿Estás seguro de agregar la calificación a la tarea?';
+    const message =
+      'Una vez guardado el registro no se podrá modificar la calificación';
+    const btnMessage = 'Agregar';
+    this.swal$.confirmationPopup(title, message, btnMessage).then((result) => {
+      if (result.isConfirmed) {
+        this.swal$.succesMessage('Curso creado con éxito');
+        this.closeModalEmmiter();
+      }
+    });
   }
 }
