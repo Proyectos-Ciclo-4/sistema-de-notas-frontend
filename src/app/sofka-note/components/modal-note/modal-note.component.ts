906296a25780ac2db066ac764a09a039041db428
Nestor Quiroga <nqs48@icloud.com>
2022-09-27 10:42:33 -0500

    Catching data value form

--- src/app/sofka-note/components/modal-note/modal-note.component.ts
+++ src/app/sofka-note/components/modal-note/modal-note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-note',
@@ -9,7 +10,15 @@ export class ModalNoteComponent implements OnInit {
   @Input() header: string = '';
   @Input() displayModal: boolean = false;
   @Output() closeModal: EventEmitter<boolean> = new EventEmitter();
+
+  formCreateCourse!: FormGroup;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  closeModalEmmiter() {
+    this.closeModal.emit(false);
+    this.formCreateCourse.reset();
+  }
 }
