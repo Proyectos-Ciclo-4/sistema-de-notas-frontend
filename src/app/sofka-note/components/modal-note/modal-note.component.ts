e11c25175b5868f05c3907199481a81b54af8acf
Nestor Quiroga <nqs48@icloud.com>
2022-09-27 14:45:37 -0500

    Adding Output, Input and eventEmiter to controller

--- src/app/sofka-note/components/modal-note/modal-note.component.ts
+++ src/app/sofka-note/components/modal-note/modal-note.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal-note',
   templateUrl: './modal-note.component.html',
-  styleUrls: ['./modal-note.component.scss']
+  styleUrls: ['./modal-note.component.scss'],
 })
 export class ModalNoteComponent implements OnInit {
+  @Input() header: string = '';
+  @Input() displayModal: boolean = false;
+  @Output() closeModal: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
