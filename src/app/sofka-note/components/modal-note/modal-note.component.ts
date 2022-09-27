5e285175b734de09a3b779e4f2d6bea259e6494f
Nestor Quiroga <nqs48@icloud.com>
2022-09-27 14:44:22 -0500

    Creating component modal-note (Calification task)

---
+++ src/app/sofka-note/components/modal-note/modal-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-note',
+  templateUrl: './modal-note.component.html',
+  styleUrls: ['./modal-note.component.scss']
+})
+export class ModalNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
